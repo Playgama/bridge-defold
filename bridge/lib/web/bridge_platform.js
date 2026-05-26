3a9f2827eca587c22452c4958790c60a3d2d8a4b
Davit <davitsedrakian@gmail.com>
2026-05-26 17:15:43 +0400

    external calls check

--- bridge/lib/web/bridge_platform.js
+++ bridge/lib/web/bridge_platform.js
@@ -67,6 +67,10 @@ let js_bridge_platform = {
         return bridge.platform.isGetGameByIdSupported;
     },
 
+    js_bridge_platform_isExternalCallsSupported: function () {
+        return bridge.platform.isExternalCallsSupported;
+    },
+
     js_bridge_platform_getAllGames: function (handler, onSuccess, onFailure) {
         bridge.platform.getAllGames()
             .then(result => {
--- bridge/src/bridge_platform.cpp
+++ bridge/src/bridge_platform.cpp
@@ -109,6 +109,10 @@ int bridge::platform::isGetGameByIdSupported(lua_State* L) {
     return getBoolean(L, js_bridge_platform_isGetGameByIdSupported);
 }
 
+int bridge::platform::isExternalCallsSupported(lua_State* L) {
+    return getBoolean(L, js_bridge_platform_isExternalCallsSupported);
+}
+
 int bridge::platform::getGameById(lua_State* L) {
     return makeCallbackWithJson(L, js_bridge_platform_getGameById, false);
 }
